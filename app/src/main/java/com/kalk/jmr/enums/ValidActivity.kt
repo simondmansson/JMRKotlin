8a9025d2cc2b43bb2b609ac7343a17ba12f2c508
Kalk88 <simondmansson@gmail.com>
2018-06-12 13:08:03 +0200

    Enums for activity broadcasts

---
+++ app/src/main/java/com/kalk/jmr/enums/ValidActivity.kt
@@ -0,0 +1,16 @@
+package com.kalk.jmr.enums
+
+/**
+ * Created by kalk on 3/27/18.
+ * The subset of activitys we use from googleActivity recognition.
+ */
+
+enum class ValidActivity {
+    IN_VEHICLE,
+    ON_BICYCLE,
+    ON_FOOT,
+    WALKING,
+    RUNNING,
+    STILL,
+    UNKNOWN
+}
