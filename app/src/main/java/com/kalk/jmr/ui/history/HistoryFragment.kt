53e9569e374e36e4fe069aad96da2caf2da691f9
Kalk88 <simondmansson@gmail.com>
2018-06-14 23:06:34 +0200

    observers playlists and updates the ui accordingly

--- app/src/main/java/com/kalk/jmr/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/kalk/jmr/ui/history/HistoryFragment.kt
@@ -8,15 +8,13 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.widget.toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.kalk.jmr.PlayCommands
 import com.kalk.jmr.R
 import kotlinx.android.synthetic.main.history_fragment.*
-import kotlinx.android.synthetic.main.history_fragment.view.*
 import kotlinx.android.synthetic.main.main_activity.*
-import kotlinx.android.synthetic.main.main_activity.view.*
 
 
 class HistoryFragment : Fragment() {
@@ -25,32 +23,13 @@ class HistoryFragment : Fragment() {
     }
 
     private lateinit var playCommands: PlayCommands
-
-    //private lateinit var viewModel:HistoryViewModel
+    private lateinit var history:HistoryViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
+        activity?.toolbar_main_text?.text = resources.getString(R.string.toolbar_history)
 
-        val view = inflater.inflate(R.layout.history_fragment, container, false)
-
-        activity?.toolbar_main_text?.text = "History"
-
-        val list = arrayListOf<Playlist>(
-                Playlist(1,"Malmö-morning-run", arrayListOf("1iGXvUsVVkYBas0Cniw6NB", "3IDsegNBHC4pjGCOMTQYlU", "2NJ3P3vXdOBk9cjPIFS3uH")),
-                Playlist(2, "Malmö-afternoon-walk", arrayListOf("50LgxH3t8vWy5xMFljdUbF", "43G2Go5tmms3DjC2qdKCD0")),
-                Playlist(3, "Malmö-morning-still", arrayListOf("4rXEQ0gVrddOY4LWUknZt3"))
-        )
-
-        view.apply {
-            history_recycler.layoutManager = LinearLayoutManager(view.context)
-            history_recycler.adapter = PlaylistAdapter(list) {
-                context.toast("${it.title} Clicked", Toast.LENGTH_SHORT)
-                val playlist = list.filter { plist ->  plist.id == it.id }
-                playCommands?.play(playlist[0].songs)
-            }
-        }
-
-        return view
+        return inflater.inflate(R.layout.history_fragment, container, false)
     }
 
     override fun onAttach(context: Context?) {
@@ -60,7 +39,21 @@ class HistoryFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProviders.of(this).get(HistoryViewModel::class.java)
-        // TODO: Use the ViewModel
+        history = ViewModelProviders.of(this).get(HistoryViewModel::class.java)
+
+        val playLists: List<Playlist> = history.playlists.value ?: listOf()
+        val adapter =  PlaylistAdapter(playLists) {
+            context?.toast("${it.title} Clicked", Toast.LENGTH_SHORT)
+            val playlist = playLists.filter { plist -> plist.id == it.id }
+            playCommands.play(playlist[0].songs)
+        }
+
+        history_recycler.layoutManager = LinearLayoutManager(context)
+        history_recycler.adapter = adapter
+
+        history.getPlaylists().observe(this, Observer(function = {
+            adapter.updatePlayList(if (it != null) it else playLists)
+        }))
+
     }
 }
