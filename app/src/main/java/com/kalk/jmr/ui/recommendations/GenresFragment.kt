7c55fa794cad3a5b46600b3a694efe0e43f45c97
Kalk88 <simondmansson@gmail.com>
2018-06-14 19:12:44 +0200

    Loads a list of genres from json, and sets the selected genre in the recommendations viewModel

--- app/src/main/java/com/kalk/jmr/ui/recommendations/GenresFragment.kt
+++ app/src/main/java/com/kalk/jmr/ui/recommendations/GenresFragment.kt
@@ -1,34 +1,30 @@
-package com.kalk.jmr.ui.genres
+package com.kalk.jmr.ui.recommendations
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.core.widget.toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.gson.Gson
 import com.kalk.jmr.R
-import kotlinx.android.synthetic.main.genre_card_view.view.*
 import kotlinx.android.synthetic.main.genres_fragment.*
-import kotlinx.android.synthetic.main.genres_fragment.view.*
 import kotlinx.android.synthetic.main.main_activity.*
-
 
 class GenresFragment : Fragment() {
     lateinit var genres:List<String>
-    lateinit var adapter:GenresAdapter
+    lateinit var adapter: GenresAdapter
 
     companion object {
         fun newInstance() = GenresFragment()
     }
 
-    //private lateinit var viewModel:HistoryViewModel
+    private lateinit var recommendations:RecommendationsViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        activity?.toolbar_main_text?.text = "Genres"
-        val view = inflater.inflate(R.layout.genres_fragment, container, false)
+        activity?.toolbar_main_text?.text = resources.getString(R.string.toolbar_genres)
 
         //Read genres from assets
         val json = context?.assets?.open(
@@ -40,14 +36,7 @@ class GenresFragment : Fragment() {
         val list = Gson().fromJson<GenresList>(json , GenresList::class.java)
         genres = list.genres.map { genre -> genre.capitalize() }
 
-        adapter = GenresAdapter(genres) { view.context.toast("${it} Clicked", Toast.LENGTH_SHORT) }
-
-        view.apply {
-            genres_recycler.layoutManager = LinearLayoutManager(view.context)
-            genres_recycler.adapter = adapter
-        }
-
-        return view
+        return inflater.inflate(R.layout.genres_fragment, container, false)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -57,13 +46,21 @@ class GenresFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        //viewModel = ViewModelProviders.of(this).get(HistoryViewModel::class.java)
-        // TODO: Use the ViewModel
+        recommendations = ViewModelProviders.of(activity!!).get(RecommendationsViewModel::class.java)
+
+        adapter = GenresAdapter(genres) {
+            context?.toast("Genre set to ${it}", Toast.LENGTH_SHORT)
+            recommendations.setGenre(it)
+        }
+
+        genres_recycler.layoutManager = LinearLayoutManager(view?.context)
+        genres_recycler.adapter = adapter
+
     }
 
     private fun filter(text: String) {
         val filtered = genres.filter { it.toLowerCase().contains(text.toLowerCase()) }
-        adapter?.filterGenres(filtered)
+        adapter.filterGenres(filtered)
     }
     
 }
