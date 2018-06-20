a80122362adb30d5491179218ca34487b815d75e
Kalk88 <simondmansson@gmail.com>
2018-06-20 13:37:34 +0200

    can find userlocation by coordinates

--- app/src/main/java/com/kalk/jmr/db/location/UserLocationDao.kt
+++ app/src/main/java/com/kalk/jmr/db/location/UserLocationDao.kt
@@ -11,6 +11,9 @@ interface UserLocationDao {
     @Query("Select * from locations where id = :id")
     fun byId(id: Int): UserLocation
 
+    @Query("Select * from locations where longitude between :longFrom and  :longTo and latitude between :latFrom and :latTo")
+    fun inRangeOfCoordinates(longFrom: Double, latFrom:Double, longTo: Double, latTo:Double): UserLocation
+
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun addLocation(location: UserLocation)
 }
