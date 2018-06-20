26ffc08eb9d536f8b70e18a57c1dfe6409626458
Kalk88 <simondmansson@gmail.com>
2018-06-20 20:44:34 +0200

    Database singelton and dao provider

---
+++ app/src/main/java/com/kalk/jmr/db/AppDatabase.kt
@@ -0,0 +1,50 @@
+package com.kalk.jmr.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.kalk.jmr.db.genre.Genre
+import com.kalk.jmr.db.genre.GenreDao
+import com.kalk.jmr.db.location.UserLocation
+import com.kalk.jmr.db.location.UserLocationDao
+import com.kalk.jmr.db.playlist.Playlist
+import com.kalk.jmr.db.playlist.PlaylistDao
+import com.kalk.jmr.db.playlistTracks.PlaylistTrack
+import com.kalk.jmr.db.playlistTracks.PlaylistTracksDao
+import com.kalk.jmr.db.track.Track
+import com.kalk.jmr.db.track.TrackDao
+import com.kalk.jmr.db.userActivity.UserActivity
+import com.kalk.jmr.db.userActivity.UserActivityDao
+
+@Database(entities = [
+    (Genre::class),
+    (UserActivity::class),
+    (UserLocation::class),
+    (Track::class),
+    (Playlist::class),
+    (PlaylistTrack::class)],
+        version = 1,
+        exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun genreDao(): GenreDao
+    abstract fun userActivityDao(): UserActivityDao
+    abstract fun locationDao(): UserLocationDao
+    abstract fun trackDao(): TrackDao
+    abstract fun playlistDao(): PlaylistDao
+    abstract fun playListTracksDao():PlaylistTracksDao
+
+    companion object {
+        private var sInstance: AppDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): AppDatabase {
+            if(sInstance == null) {
+                sInstance = Room.databaseBuilder(context.applicationContext,
+                        AppDatabase::class.java, AppDatabase::class.java.simpleName).build()
+            }
+            return sInstance!!
+        }
+    }
+}
