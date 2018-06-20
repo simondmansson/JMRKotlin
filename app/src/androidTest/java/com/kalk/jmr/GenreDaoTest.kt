e6dceac534243c6c7efdf3e0edcc4542b21cf32a
Kalk88 <simondmansson@gmail.com>
2018-06-20 11:53:11 +0200

    Test for conflicting values, conflicts are handled silently for now so it's up to the application to make sure Id's are unique. Genres should only be added once so ignoring conflicts should be safe.

--- app/src/androidTest/java/com/kalk/jmr/GenreDaoTest.kt
+++ app/src/androidTest/java/com/kalk/jmr/GenreDaoTest.kt
@@ -7,6 +7,7 @@ import com.kalk.jmr.db.AppDatabase
 import com.kalk.jmr.db.genre.Genre
 import com.kalk.jmr.db.genre.GenreDao
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -31,5 +32,33 @@ class GenreDaoTest {
         val queryResult = dao.byId(1)
         assertEquals(queryResult.id, grunge.id)
         assertEquals(queryResult.genre, grunge.genre)
+    }
+
+
+    @Test
+    fun cannot_insert_genre_with_same_id() {
+        val grunge = Genre(1, "grunge")
+        val rock = Genre(1, "rock")
+        dao.addGenre(grunge)
+        dao.addGenre(rock)
+        val queryResult = dao.byId(1)
+        assertEquals(queryResult.id, grunge.id)
+        assertEquals(queryResult.genre, grunge.genre)
+        assertNotEquals(queryResult.genre, rock.genre)
+    }
+
+
+    @Test
+    fun cannot_insert_genre_with_same_type_of_genre() {
+        val grunge = Genre(1, "grunge")
+        val grunge2 = Genre(2, "grunge")
+        dao.addGenre(grunge)
+        dao.addGenre(grunge2)
+        val queryResult = dao.byId(1)
+        assertEquals(queryResult.id, grunge.id)
+        assertEquals(queryResult.genre, grunge.genre)
+        val queryResult2 = dao.byId(2)
+        assertEquals(queryResult2, null)
+
     }
 }
