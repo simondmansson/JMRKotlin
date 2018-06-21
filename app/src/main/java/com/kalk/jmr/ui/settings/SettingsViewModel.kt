791289c1471c12847046bbf445f0c3b90da00269
Kalk88 <simondmansson@gmail.com>
2018-06-21 17:49:06 +0200

    init

--- app/src/main/java/com/kalk/jmr/ui/settings/SettingsViewModel.kt
+++ app/src/main/java/com/kalk/jmr/ui/settings/SettingsViewModel.kt
@@ -1,11 +1,17 @@
 package com.kalk.jmr.ui.settings
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.arch.lifecycle.MutableLiveData
+import android.arch.lifecycle.ViewModel
 
 class SettingsViewModel: ViewModel() {
 
     val time: MutableLiveData<Boolean> = MutableLiveData()
     val location: MutableLiveData<Boolean> = MutableLiveData()
     val activity: MutableLiveData<Boolean> = MutableLiveData()
+
+    init {
+        time.value = false
+        location.value = false
+        activity.value = false
+    }
 }
