cf912d7fd5478c7c1c3ca649ae15327cbf44c2d2
Kalk88 <simondmansson@gmail.com>
2018-06-24 19:07:02 +0200

    some location tests

--- app/src/test/java/com/kalk/jmr/UtilityTests.kt
+++ app/src/test/java/com/kalk/jmr/UtilityTests.kt
@@ -1,14 +1,20 @@
 package com.kalk.jmr
 
+import com.kalk.jmr.db.location.Coordinates
+import com.kalk.jmr.db.location.UserLocation
 import com.kalk.jmr.ui.recommendations.Token
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Ignore
 import org.junit.Test
 
 class UtilityTests {
     val threeHours = 10_800_000L
     val fortyFiveMinutes = 2_700_000L
+    val fifteenMinutes = 900_000L
     val tokenString = "abc"
+    val dummyLoc = UserLocation(1, coordinates = Coordinates(0.0,0.0))
+
     @Test
     fun should_Request_New_Token_when_string_is_empty() {
         assertTrue(shouldRequestNewToken(Token("", 0), threeHours))
@@ -22,5 +28,27 @@ class UtilityTests {
     @Test
     fun should_not_Request_New_Token_when_time_passed_is_less_than_55min() {
         assertFalse(shouldRequestNewToken(Token(tokenString, 3_240_000L), fortyFiveMinutes))
+    }
+
+    @Test
+    fun should_request_new_location_when_time_passed_is_less_than_fifteenMinutes() {
+        shouldRequestNewLocation(dummyLoc, 800_000L, fifteenMinutes)
+    }
+
+    @Test
+    fun should_request_new_location_when_time_passed_is_greater_than_fifteenMinutes() {
+        shouldRequestNewLocation(dummyLoc, 1000_000L, fifteenMinutes)
+    }
+
+    @Test
+    @Ignore
+    fun should_request_new_location_when_long_distance_is_greater_than_() {
+
+    }
+
+    @Test
+    @Ignore
+    fun should_request_new_location_when_lat_distance_is_greater_than_() {
+
     }
 }
